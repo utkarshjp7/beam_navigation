984ee50788dbe91ad95aebca51e6e20a167f1538
ehatay <emrehatay7@gmail.com>
2017-05-02 16:30:09 -0400

    modified:   beam_2dnav/CMakeLists.txt 	deleted:    beam_2dnav/scripts/sendEmailSrv.py 	deleted:    beam_2dnav/scripts/speak.py 	deleted:    beam_2dnav/scripts/twitter_checker_node.py 	modified:   beam_tf/src/tf_broadcaster.cpp

--- beam_tf/src/tf_broadcaster.cpp
+++ beam_tf/src/tf_broadcaster.cpp
@@ -2,28 +2,24 @@
 #include <tf/transform_broadcaster.h>
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "beam_tf_publisher");
-  ros::NodeHandle n;
+	ros::init(argc, argv, "beam_tf_publisher");
+	ros::NodeHandle n;
 
-  ros::Rate r(30);
+	ros::Rate r(30);
 
-  tf::TransformBroadcaster broadcaster;
+	tf::TransformBroadcaster broadcaster;
 
-  float ori = -500;
-  while(n.ok()){
-    broadcaster.sendTransform(
-      tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1778, 0.0, 0.1395)),
-        ros::Time::now(),"/base_link", "/asus_link"));
-    broadcaster.sendTransform(
-      tf::StampedTransform(
-<<<<<<< HEAD
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.1478, 0.0, 1.3208)),
-=======
-        tf::Transform(tf::Quaternion(0, 0, ori, 1), tf::Vector3(-0.1478, 0.0, 0.1450)),
->>>>>>> 285d6768db49fe3900c8295621111a9e3ca9677a
-        ros::Time::now(),"/base_link", "/kinect_link"));
-    r.sleep();
-  }
+	float ori = -500;
+	while(n.ok()){
+		broadcaster.sendTransform(
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1778, 0.0, 0.1395)),
+				ros::Time::now(),"/base_link", "/asus_link"));
+		broadcaster.sendTransform(
+			tf::StampedTransform(
+				tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.1478, 0.0, 1.3208)),
+				ros::Time::now(),"/base_link", "/kinect_link"));
+		r.sleep();
+	}
 }
 //-0.1478
